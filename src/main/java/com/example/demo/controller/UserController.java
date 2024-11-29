919ca9789e13205a45008b6ad5b721ec212105d5
Uditha <udithachathuranga99@gmail.com>
2024-11-29 19:42:39 +0530

    UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,40 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping(value="api/v1")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/user/{userId}")
+    public UserDTO getUserById(@PathVariable Integer userId) {
+        return userService.getUserById(userId);
+    }
+
+    @PostMapping("/addUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteuser")
+    public String deleteUser(Integer userId){
+        return userService.deleteUser(userId);
+    }
+
 }
